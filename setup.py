02e8ae7b70aa6786e3ecaf3742ea517b54154aa4
John Friedman <jgfriedman99@gmail.com>
2026-05-28 15:15:30 -0700

    Bump version and make PyPI upload verbose

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="datamulehub",
     author="John Friedman",
-    version="0.1.0",
+    version="0.1.1",
     description="Access Datamule cloud",
     url="https://github.com/john-friedman/datamule-hub",
     packages=find_packages(),
