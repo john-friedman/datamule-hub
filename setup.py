9b7d3ff1efba9898f2209b907b3107f42fec96c5
John Friedman <jgfriedman99@gmail.com>
2026-06-25 13:28:52 -0700

    fix v number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="datamule-hub",
     author="John Friedman",
-    version="0.2.0o",
+    version="0.2.0",
     description="Access Datamule cloud",
     url="https://github.com/john-friedman/datamule-hub",
     packages=find_packages(),
